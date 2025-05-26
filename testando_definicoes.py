aa8d09a129d50756c8725bb5e68d786618df3b44
Luís Arthur Turck da Silva <luisarthur0207@gmail.com>
2025-05-26 15:24:18 -0400

    Add files via upload

--- testando_definicoes.py
+++ testando_definicoes.py
@@ -1,21 +1,43 @@
-#testando definições
-#listas:
-compras = ["pao","cafe","leite"]
-print(compras)
+# testando definições
+# listas:
+# compras = ["pao","cafe","leite"]
+# print(compras)
 
 #pode ser removido pelo nome ou pelo indice
 #compras.remove("cafe")
-compras.remove(compras[0])
-print(compras)
+# compras.remove(compras[0])
+# print(compras)
 
-#append acreçenta um item ao final da lista, só pode adicionar um por vez
-compras.append("açucar")
-print(compras)
+# append acreçenta um item ao final da lista, só pode adicionar um por vez
+# compras.append("açucar")
+# print(compras)
 
-#é preciso criar uma lista nova para receber os elementos ordenados
-compras_ordenadas=sorted(compras)
-print(compras_ordenadas)
+# é preciso criar uma lista nova para receber os elementos ordenados
+# compras_ordenadas=sorted(compras)
+# print(compras_ordenadas)
 
-#o nome item é um identificador dos itens dentro da lista, podendo receber qualquer nome
-for item in compras:
-    print(f"-{item}")
+# o nome item é um identificador dos itens dentro da lista, podendo receber qualquer nome
+# for item in compras:
+#       print(f"-{item}")
+
+#listas= []
+#dicionrios= {chave:valor}
+#tuplas= ()
+#conjuntos= set()
+
+# teste=[1,2,3,5]
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+#da errado pois yuplas são imutaveis
+# teste=(1,2,3,5)
+# print(teste)
+# teste.remove(1)
+# print(teste)
+
+# teste={1,2,3,5,5,6,6}
+# print(teste)
+# teste.remove(1)
+# print(teste)
+
